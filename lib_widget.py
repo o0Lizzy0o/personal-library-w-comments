8da6ed305c74e1f1e056d1b52ade4e03cb553a5b
o0Lizzy0o <lizavictoriang@gmail.com>
2026-06-22 16:35:26 +0100

    toggle blueprints

--- lib_widget.py
+++ lib_widget.py
@@ -20,6 +20,27 @@ root.geometry('700x450')
 input1=StringVar()
 
 
+#blueprint for grid/list view toggles and orderings
+is_on = False
+
+def toggle():
+     global is_on
+     is_on = not is_on
+     if is_on:
+          listView.config(bg='red')
+          #conn = sqlite3.connect('manga.db')
+          #c=conn.cursor()
+          #c.execute("SELECT status FROM Status")
+          #rows= c.fetchall()
+          #for row in rows :
+          #     print(row)
+          #conn.close()
+     else:
+          listView.config(bg='green')
+
+listView = Button(root,bg = 'green' ,command=toggle)
+listView.pack()
+
 class My_widget(Frame):
 
      def __init__(self, parent, label_text, button_text):
@@ -32,7 +53,7 @@ class My_widget(Frame):
           #buttons
           self.label = Label(self, text=label_text)
           self.label.grid(row=0, column=0)
-          Button(self, text=button_text,command=self.input).grid(row=0, column=1)
+          Button(self, text=button_text,command=self.input,fg="white",bg="red").grid(row=0, column=1)
           #inputbox example
           Entry(self,textvariable=input1).grid(row=0, column=2)
           self.pack()
@@ -40,6 +61,7 @@ class My_widget(Frame):
      def input(self) :
           entryInput = input1.get()
           self.label.config(text=entryInput)
+          #print(entryInput)
 
 My_widget(root,"Label 1","Button 1")
 
